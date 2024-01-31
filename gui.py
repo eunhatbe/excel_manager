b69299240e5aeb2f28f33073e5077945ecccbb03
eunhatbe <101296387+eunhatbe@users.noreply.github.com>
2024-01-31 16:50:06 +0900

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,34 +1,36 @@
 import sys
 
+from PyQt5.QtCore import QDate, pyqtSignal, QObject, pyqtSlot
 from PyQt5.QtWidgets import QApplication, QWidget, QDesktopWidget, QFileDialog, QPushButton, QVBoxLayout, QHBoxLayout, \
     QMessageBox, QLabel, QCalendarWidget, QLineEdit
 
 
 class CalendarWindow(QWidget):
-
-    # send_date = pySignal
+    send_date = pyqtSignal(str)
 
     def __init__(self):
         super().__init__()
 
         self.date = None
-
         self.setWindowTitle("Calendar")
-        self.initUI()
+        self.init_ui()
 
-    def initUI(self):
-        vbox = QVBoxLayout()
+    def init_ui(self):
         self.calendar = QCalendarWidget(self)
+        self.calendar.setGridVisible(True)
+
+        vbox = QVBoxLayout()
         vbox.addWidget(self.calendar)
         self.setLayout(vbox)
 
+        self.calendar.clicked.connect(self.select_date)
 
-
-
+    def select_date(self):
+        self.date = self.calendar.selectedDate().toString('yyyy-MM-dd')
+        self.send_date.emit(self.date)
 
 
 class App(QWidget):
-
     def __init__(self):
         super().__init__()
 
@@ -37,6 +39,7 @@ class App(QWidget):
         self.save_url = None
 
         self.date_data = None
+        self.calendar_window = None
 
         self.init_ui()
 
@@ -106,7 +109,7 @@ class App(QWidget):
         self.date_edit.move(180,20)
 
         # 버튼 이벤트 초기화
-        self.select_base_button.clicked.connect(lambda: self.open_directory_dialog("base", self.basepath_label))
+        self.select_base_button.clicked.connect(lambda: self.open_file_dialog(self.basepath_label))
         self.select_text_button.clicked.connect(lambda: self.open_directory_dialog("text", self.textpath_label))
         self.select_save_button.clicked.connect(lambda: self.open_directory_dialog("save", self.savepath_label))
 
@@ -116,29 +119,36 @@ class App(QWidget):
         self.show()
 
     def center_window_onscreen(self):
-        frameGeometry = self.frameGeometry()
-        screenCenterPoint = QDesktopWidget().availableGeometry().center()
-        frameGeometry.moveCenter(screenCenterPoint)
-        self.move(frameGeometry.topLeft())
+        frame_geometry = self.frameGeometry()
+        screen_center_point = QDesktopWidget().availableGeometry().center()
+        frame_geometry.moveCenter(screen_center_point)
+        self.move(frame_geometry.topLeft())
 
     def open_directory_dialog(self, m_url: str, label: QLabel):
+        folder_path = QFileDialog.getExistingDirectory(self)
+        label.setText(folder_path)
 
+        if m_url == "text":
+            self.text_url = folder_path
+        elif m_url == "save":
+            self.save_url = folder_path
+
+    def open_file_dialog(self, label: QLabel):
         file_name = QFileDialog.getOpenFileName(self)
         url = file_name[0]
         label.setText(url)
 
-        if m_url == "base":
-            self.base_url = url
-        elif m_url == "text":
-            self.text_url = url
-        elif m_url == "save":
-            self.save_url = url
-
     def select_date_event(self):
-        self.calendarWindow = CalendarWindow()
-        self.calendarWindow.show()
+        self.calendar_window = CalendarWindow()
+        self.calendar_window.send_date.connect(self.test_fun)
 
+        self.calendar_window.show()
 
+    @pyqtSlot(str)
+    def test_fun(self, date):
+        self.date_data = date
+        self.date_edit.setText(self.date_data)
+        self.calendar_window.deleteLater()
 
 
 if __name__ == '__main__':
